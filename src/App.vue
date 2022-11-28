27429d7bb4a2a7b1c7e5e52a54b26bdeda898933
HyoBeom <hyobeom@codemind.co.kr>
2022-11-28 16:21:37 +0900

    feat: get ip

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 
 <template>
+  <Coodinate brand="naver"/>
 </template>
 <script setup lang="ts">
+import Coodinate from "./components/Coordinate.vue"
 </script>
 <style scoped>
 </style>
--- src/components/Coordinate.vue
+++ src/components/Coordinate.vue
@@ -3,13 +3,11 @@
 <script lang='ts'>
 import { defineComponent } from 'vue'
 import gql from 'graphql-tag'
-
+import moment from 'moment';
 export default defineComponent({
   name: 'Coordinate',
   props: {
     brand: String,
-    ip: String,
-    date: String,
   },
   data () {
     return {
@@ -18,10 +16,14 @@ export default defineComponent({
         x:50,
         y:50
       },
-      intervalId: 0
+      intervalId: 0,
+      date: moment().format("YYYY-MM-DD HH:mm:ss"),
+      ip: 0
     }
   },
-  created (){
+  async created (){
+    const { ip } = await fetch('https://api.ipify.org?format=json').then(x => x.json());
+    this.ip = ip;
     window.addEventListener('beforeunload', this.setMouseCoordinate);
     document.addEventListener("mousemove", (event) => {
       this.currnetCoordinate = {
@@ -42,14 +44,14 @@ export default defineComponent({
             variables: {
                 cursorMonitor: {
                   brand: this.brand,
-                  ip: this.ip,
-                  date: this.date,
-                  coordinate: this.coordinate
+                  date: moment().format("YYYY-MM-DD HH:mm:ss"),
+                  coordinate: this.coordinate,
+                  ip: this.ip
             },
           }
           })
           window.clearInterval(this.intervalId);
-        }
+        },
     }
 })
 </script>
